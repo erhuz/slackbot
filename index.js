5bf910f55cfa810d3a10df1250c109145668c68e
Benjamin Fransson <benjamin@cafcon.se>
2019-04-16 10:05:41 +0200

    Added and implemented winstonjs as logger

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const dotenv = require('dotenv').config();
+const logger = require('./logger');
 
-console.log(process.env.NODE_ENV)
+logger.info('This is sent to a combined.log file');
+logger.error('This is sent to a error.log file');
---
+++ logger.js
@@ -0,0 +1,43 @@
+const winston = require('winston');
+const { createLogger, format, transports } = winston;
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  format: combine(
+    label({ label: 'right meow!' }),
+    timestamp(),
+    myFormat
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console({ level: 'debug' })
+  ]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+module.exports = logger;
+
+/*
+ * Logs with level debug should be printed to the console.
+ * Logs with level info should be written to a file called combined.log.
+ * Logs with level error should be written to a file called error.log.
+*/
